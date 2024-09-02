0c453647cc319e48065727f1957e89b6b5be79a2
성범 이 <crane2356@naver.com>
2024-09-02 11:33:26 +0900

    Update Video.js
    - Remove <div> in CContainer

--- frontend/src/views/pages/settings/Video.js
+++ frontend/src/views/pages/settings/Video.js
@@ -6,21 +6,19 @@ const Video = () => {
   return (
     <CRow className="justify-content-center">
       <CContainer fluid className="p-0 m-0" style={{ height: '100vh' }}>
-        <div style={{ height: '100vh', width: '100vw', margin: 0, padding: 0 }}>
-          <video
-            src={VideoFile}
-            style={{
-              height: '100%',
-              width: '100%',
-              margin: 0,
-              padding: 0,
-            }}
-            controls
-            autoPlay
-            loop
-            muted
-          />
-        </div>
+        <video
+          src={VideoFile}
+          style={{
+            height: '100%',
+            width: '100%',
+            margin: 0,
+            padding: 0,
+          }}
+          controls
+          autoPlay
+          loop
+          muted
+        />
       </CContainer>
     </CRow>
   )
