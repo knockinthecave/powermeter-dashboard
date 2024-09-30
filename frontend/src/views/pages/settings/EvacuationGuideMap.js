ab9018abef506116926c54d4e3503e64b295e0d1
성범 이 <crane2356@naver.com>
2024-09-30 15:52:06 +0900

    Update environments variables
    - change DB information for no internet circumstances

--- frontend/src/views/pages/settings/EvacuationGuideMap.js
+++ frontend/src/views/pages/settings/EvacuationGuideMap.js
@@ -1,26 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { CRow, CContainer } from '@coreui/react-pro'
-import evacuationGuideMap from 'src/assets/images/settings/evacuation_route/evacuation.jpg'
+import Evauation from 'src/assets/images/settings/evacuation_route/Evacuation.png'
 import './EvacuationGuideMap.css' // CSS 파일로 스타일 추가
 
 const EvacuationGuideMap = () => {
-  const [currentIndex, setCurrentIndex] = useState(0)
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % 2) // 예시로 두 가지 상태로 깜빡임
-    }, 500) // 0.5초마다 깜빡임
-
-    return () => clearInterval(intervalId)
-  }, [])
-
   return (
     <CRow className="justify-content-center">
       <CContainer fluid className="p-0 m-0 evacuation-map-container">
         <div
-          className={`evacuation-map-background ${currentIndex === 0 ? 'blink-on' : 'blink-off'}`}
           style={{
-            backgroundImage: `url(${evacuationGuideMap})`,
+            backgroundImage: `url(${Evauation})`,
             backgroundSize: 'contain',
             backgroundPosition: 'center',
             backgroundRepeat: 'no-repeat',
